d5057e41f13547a76e5c6461bf537e264ab5d0ed
Zhuyun DAI <dzy197810@hotmail.com>
2014-07-04 10:47:34 +0000

    Multiple changes

--- proxies/questionLibrary.js
+++
@@ -1,6 +0,0 @@
-﻿var models = require('../models');
-var QuestionLibrary = models.QuestionLibrary;
-
-module.exports.getAllQuestionLibraryNames = function(callback) {
-
-};
---
+++ tools/launchEnv2.cmd
@@ -0,0 +1,4 @@
+﻿cd G:\ZDai\GitHub\InterExam
+start cmd /c mongod --dbpath G:\ZDai\MongoDB\data --logpath G:\ZDai\MongoDB\log\db.log --logappend
+start cmd /k nodevars.bat 
+start cmd /c mongo localhost/interexam
