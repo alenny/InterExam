2910b8bdc0b3fb5b701503dccd580f6d45f6104b
Zhuyun DAI <dzy197810@hotmail.com>
2014-07-08 07:07:16 +0000

    Refactor UI

--- public/js/controllers/eat.js
+++ public/js/controllers/eat.js
@@ -1,8 +1,28 @@
-﻿function EatController($scope, $http) {
+﻿var appModule = angular.module('ieApp', []);
+
+appModule.controller('EatController', function($scope, $http) {
     $scope.restaurants = [];
     $scope.recommended = {};
-    getAllRestaurants($scope, $http);
-    $scope.addRestaurant = function() {
+
+    var classes = ['alert-success', 'alert-warning', 'alert-info', 'alert-danger'];    
+    
+    function getRestaurantClass(idx) {
+        return 'alert ' + classes[idx % classes.length];
+    }
+    
+    function getAllRestaurants() {
+        $http({
+            url: '/eat/restaurants',
+            method: 'GET',
+        }).success(function (data) {
+            $scope.restaurants = data.reverse();
+            for (var i = 0; i < $scope.restaurants.length; ++i) {
+                $scope.restaurants[i].styleClass = getRestaurantClass(i);
+            }
+        });
+    }
+        
+    $scope.addRestaurant = function () {
         if (!$scope.newRestaurantName) {
             return;
         }
@@ -11,34 +31,27 @@
             method: 'POST',
             data: { name: $scope.newRestaurantName }
         }).success(function() {
-            getAllRestaurants($scope, $http);
+            getAllRestaurants();
         });
         $scope.newRestaurantName = '';
     };
+    
     $scope.removeRestaurant = function (id) {
         $http({
             url: '/eat/restaurants/' + id,
             method: 'DELETE'
         }).success(function() {
-            getAllRestaurants($scope, $http);
+            getAllRestaurants();
         });
     };
+    
     $scope.recommendOne = function () {
         if (!$scope.restaurants) {
             return;
         }
         var idx = Math.floor(Math.random() * $scope.restaurants.length);
         $scope.recommended = $scope.restaurants[idx];
-        $('#recommendedRestaurant').addClass('in');
     };
-}
-
-function getAllRestaurants($scope, $http) {
-    $http({
-        url: '/eat/restaurants',
-        method: 'GET',
-    }).success(function (data) {
-        $scope.restaurants = data.reverse();
-    });
-}
-
+    
+    getAllRestaurants();
+});
